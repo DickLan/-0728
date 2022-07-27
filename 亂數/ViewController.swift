b0ea16c7ba5ad707c06db70ed158a88dc0bb26ba
BBC <mianwash5566@gmail.com>
2022-07-27 00:36:58 -0700

    Initial Commit

---
+++ 亂數/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  亂數
+//
+//  Created by Lan on 2022/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
